8e207aa7a47a9a35f76e0fdc532f3a4704325798
sloth <andinhtran123@gmail.com>
2025-06-22 21:11:52 +0700

    Fixed email sitelink

--- users/models.py
+++ users/models.py
@@ -19,7 +19,7 @@ class CustomUser(AbstractUser):
 @receiver(reset_password_token_created)
 def password_reset_token_created(reset_password_token, *args, **kwargs):
     # sitelink = "http://localhost:5173/"
-    sitelink = "https://lecturer-management-frontend.onrender.com"
+    sitelink = "https://lecturer-management-frontend.onrender.com/"
     token = "{}".format(reset_password_token.key)
     full_link = f"{sitelink}password_reset/{token}"
     context = {
